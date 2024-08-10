29f00c73857a80da352e337457e9e889ee4caff1
Nifemi-git <nifemiakingba@gmail.com>
2024-08-10 23:58:17 +0100

    correct images and img url

--- src/components/Promotions.tsx
+++ src/components/Promotions.tsx
@@ -1,29 +1,4 @@
 import "../styles/promotions.css";
-interface OfferInterface {
-  text: string;
-  image: string;
-}
-
-const offersList: OfferInterface[] = [
-  { text: "Value Meals made for you", image: "present.png" },
-  { text: "Takeout or Dine-in", image: "present.png" },
-  { text: "Biggest Offer on big order", image: "present.png" },
-];
-
-const Offer = ({ text, image }: OfferInterface) => {
-  return (
-    <div className="offer-container">
-      <div className="offer-images">
-        <img src={`${import.meta.env.BASE_URL}sparkles.png`} alt="sparkles" />
-        <img src={`${import.meta.env.BASE_URL}${image}`} alt="graphic" />
-      </div>
-
-      <div className="offer-text">
-        <p>{text}</p>
-      </div>
-    </div>
-  );
-};
 
 const Promotions = () => {
   return (
@@ -35,9 +10,15 @@ const Promotions = () => {
 
       {/* bottom row */}
       <div className="promotions--bottom-div">
-        {offersList.map((offer) => (
-          <Offer text={offer.text} image={offer.image} />
-        ))}
+        <div>
+          <img src={`${import.meta.env.BASE_URL}offer-1.png`} alt="offer" />
+        </div>
+        <div>
+          <img src={`${import.meta.env.BASE_URL}offer-2.png`} alt="offer" />
+        </div>
+        <div>
+          <img src={`${import.meta.env.BASE_URL}offer-3.png`} alt="offer" />
+        </div>
       </div>
     </div>
   );
